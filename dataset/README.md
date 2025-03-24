65cae04e46c88eed08cf568bb1b81a070e98bea1
Someon1e <142684596+Someon1e@users.noreply.github.com>
2025-03-24 15:37:03 +0000

    feat: Relabel dataset again Bench: 21685453

--- dataset/README.md
+++ dataset/README.md
@@ -3,11 +3,11 @@ lichess-big3-resolved self-relabeled
 Parameters:
 
 ```
-engine bench: 23957077
+engine bench: 21685453
 
 const CHUNK_SIZE: usize = 1 << 19;
-const HARD_NODE_LIMIT: u64 = 400_000;
-const SOFT_NODE_LIMIT: u64 = 150_000;
+const HARD_NODE_LIMIT: u64 = 700_000;
+const SOFT_NODE_LIMIT: u64 = 250_000;
 
 const WIN_THRESHOLD: EvalNumber = 100;
 ```
--- relabel/src/main.rs
+++ relabel/src/main.rs
@@ -20,8 +20,8 @@ use encrustant::search::time_manager::{NodeLimit, TimeManager};
 use encrustant::search::transposition::megabytes_to_capacity;
 
 const CHUNK_SIZE: usize = 1 << 19;
-const HARD_NODE_LIMIT: u64 = 400_000;
-const SOFT_NODE_LIMIT: u64 = 150_000;
+const HARD_NODE_LIMIT: u64 = 700_000;
+const SOFT_NODE_LIMIT: u64 = 250_000;
 
 const WIN_THRESHOLD: EvalNumber = 100;
 
